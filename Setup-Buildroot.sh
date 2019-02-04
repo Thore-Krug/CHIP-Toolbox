c0e9398c3f11f295c9fa18cf4bfb8d5840ac660b
Thore-Krug <36414231+Thore-Krug@users.noreply.github.com>
2019-02-04 01:16:28 +0100

    Update Setup-Buildroot.sh

--- Setup-Buildroot.sh
+++ Setup-Buildroot.sh
@@ -2,29 +2,29 @@
 
 sudo apt update 
 sudo apt-get -y install \
- which \
- sed \
- make \
- binutils \
- build-essential \
- gcc\
- g++ \
- bash \
- patch \
- gzip \
- bzip2 \
- perl \
- tar \
- cpio \ 
- python \
- unzip \ 
- rsync \
- file\
- bc \ 
- wget \ 
- ncurses5  \ 
- glib2 \ 
- gtk2 \ 
- glade2 \ 
- git \
+  which \
+  sed \
+  make \
+  binutils \
+  build-essential \
+  gcc\
+  g++ \
+  bash \
+  patch \
+  gzip \
+  bzip2 \
+  perl \
+  tar \
+  cpio \ 
+  python \
+  unzip \ 
+  rsync \
+  file\
+  bc \ 
+  wget \ 
+  ncurses5 \ 
+  glib2 \ 
+  gtk2 \ 
+  glade2 \ 
+  git \
  
